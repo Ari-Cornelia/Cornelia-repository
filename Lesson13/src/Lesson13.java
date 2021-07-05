8bbbed403b75d1afdce8ec4ac6db388d45b612f9
Ari-Cornelia <volosin1997@gmail.com>
2021-07-05 16:49:29 +0300

    update

---
+++ Lesson13/src/Lesson13.java
@@ -0,0 +1,11 @@
+public class Lesson13 {
+
+    public static void main (String[] args){
+        String a = "Cornelia";
+        char[] b = a.toCharArray();
+        for(char i : b) {
+            System.out.println(i);
+        }
+    }
+}
+
